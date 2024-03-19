46e964a3dcc30100fa44163dec93414d52895f79
kareem7199 <kareemtameregy@gmail.com>
2024-03-19 23:04:06 +0200

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ Route.Session3.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Route.Session3.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=KAREEM\\MSSQLSERVER04;Initial Catalog=MVCApplication;Integrated Security=True; TrustServerCertificate = true", x => x.UseDateOnlyTimeOnly());
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
