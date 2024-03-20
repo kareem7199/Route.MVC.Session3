9622e5d2a32d4b7e464a6eefe18bc58609b61d40
kareem7199 <kareemtameregy@gmail.com>
2024-03-20 02:45:05 +0200

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ Route.Session3.BLL/Interfaces/IDepatmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.Session3.DAL.Models;
+
+namespace Route.Session3.BLL.Interfaces
+{
+    internal interface IDepatmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department Get(int id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+    }
+}
---
+++ Route.Session3.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Route.Session3.BLL.Interfaces;
+using Route.Session3.DAL.Data;
+using Route.Session3.DAL.Models;
+
+namespace Route.Session3.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepatmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Department entity)
+        {
+            _dbContext.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+            ///var department = _dbContext.Departments.Local.Where(D => D.Id == id).FirstOrDefault();
+            ///if(department == null)
+            ///    department = _dbContext.Departments.Where(D => D.Id == id).FirstOrDefault();
+            ///return department;
+            return _dbContext.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+            => _dbContext.Departments.AsNoTracking().ToList();
+
+        public int Update(Department entity)
+        {
+            _dbContext.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
