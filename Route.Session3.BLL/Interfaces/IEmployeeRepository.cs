a0ab36627fdd83479955c7179a7f8b4e43eb0ec9
kareem7199 <kareemtameregy@gmail.com>
2024-03-23 16:53:36 +0200

    Part 06 Employee Repository - BLL

---
+++ Route.Session3.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.Session3.DAL.Models;
+
+namespace Route.Session3.BLL.Interfaces
+{
+    internal interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee Get(int id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
---
+++ Route.Session3.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Route.Session3.DAL.Data;
+using Route.Session3.DAL.Models;
+
+namespace Route.Session3.BLL.Repositories
+{
+    internal class EmployeeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public EmployeeRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Employee entity)
+        {
+            _dbContext.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+        {
+            ///var Employee = _dbContext.Employees.Local.Where(D => D.Id == id).FirstOrDefault();
+            ///if(Employee == null)
+            ///    Employee = _dbContext.Employees.Where(D => D.Id == id).FirstOrDefault();
+            ///return Employee;
+            return _dbContext.Employees.Find(id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+            => _dbContext.Employees.AsNoTracking().ToList();
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
